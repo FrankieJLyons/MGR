9cb14a31f7b7da0d9722bec403bec1cb89572497
Frankie <frankiejlyons@gmail.com>
2022-08-03 23:11:58 +0100

    basic sprite

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,80 @@
-use bevy::{prelude::*, render::camera::ScalingMode, window::PresentMode};
+use bevy::{prelude::*, render::texture::ImageSettings};
 
-pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
 pub const RESOLUTION: f32 = 16.0 / 9.0;
 
 fn main() {
-    let height = 450.0;
+    let height = 720.0;
     App::new()
-        .insert_resource(ClearColor(CLEAR))
+        .insert_resource(ImageSettings::default_nearest())
         .insert_resource(WindowDescriptor {
             width: height * RESOLUTION,
             height: height,
-            title: "Metal Gear".to_string(),
-            present_mode: PresentMode::AutoNoVsync,
+            title: "Bevy Tutorial".to_string(),
+            resizable: false,
             ..Default::default()
-        })
-        .add_startup_system(spawn_camera)
+        }) // prevents blurry sprites
         .add_plugins(DefaultPlugins)
+        .add_startup_system(spawn_camera)
+        .add_startup_system(setup)
+        .add_system(animate_sprite)
         .run();
+}
+
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
+fn animate_sprite(
+    time: Res<Time>,
+    texture_atlases: Res<Assets<TextureAtlas>>,
+    mut query: Query<(
+        &mut AnimationTimer,
+        &mut TextureAtlasSprite,
+        &Handle<TextureAtlas>,
+    )>,
+) {
+    for (mut timer, mut sprite, texture_atlas_handle) in &mut query {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
+            sprite.index = (sprite.index + 1) % texture_atlas.textures.len();
+        }
+    }
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture_handle = asset_server.load("snake/snake_walking.png");
+    let texture_atlas = TextureAtlas::from_grid_with_padding(
+        texture_handle,
+        Vec2::new(16.0, 29.0),
+        3,
+        1,
+        Vec2::splat(1.0),
+        Vec2::splat(1.0),
+    );
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    commands.spawn_bundle(Camera2dBundle::default());
+    commands
+        .spawn_bundle(SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            transform: Transform::from_scale(Vec3::splat(6.0)),
+            ..default()
+        })
+        .insert(AnimationTimer(Timer::from_seconds(0.25, true)));
 }
 
 fn spawn_camera(mut commands: Commands) {
     let mut camera = Camera2dBundle::default();
+
+    //Set the camera to have normalized coordinates of y values -1 to 1
     camera.projection.top = 1.0;
     camera.projection.bottom = -1.0;
-    camera.projection.top = 1.0 * RESOLUTION;
-    camera.projection.top = -1.0 * RESOLUTION;
-    camera.projection.scaling_mode = ScalingMode::None;
+
+    camera.projection.right = 1.0 * RESOLUTION;
+    camera.projection.left = -1.0 * RESOLUTION;
+
     commands.spawn_bundle(camera);
 }
