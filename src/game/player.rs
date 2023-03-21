905f9d91bda1a45af94388ed969f574fb8fd5b0f
Frankie <frankiejlyons@gmail.com>
2023-03-21 22:31:46 +0000

    draw snake scaled and filtered

--- src/game/player.rs
+++ src/game/player.rs
@@ -13,6 +13,31 @@ impl Player {
     pub fn update(&mut self) {}
 
     pub fn draw(&self) {
-        draw_texture(self.texture, 0.0, 0.0, WHITE);
+        let frame_width = self.texture.width() / 4.0 + 4.0; // Assuming 4 frames in the sprite sheet
+        let frame_height = self.texture.height() / 4.0 + 2.0;
+
+        let scale = Vec2::new(4.0, 4.0);
+        let dest_rect = Rect::new(
+            (screen_width() - frame_width * scale.x) / 2.0,
+            (screen_height() - frame_height * scale.y) / 2.0,
+            frame_width * scale.x,
+            frame_height * scale.y,
+        );
+
+        let src_rect = Rect::new(0.0, 0.0, frame_width, frame_height);
+
+        self.texture.set_filter(FilterMode::Nearest);
+
+        draw_texture_ex(
+            self.texture,
+            dest_rect.x,
+            dest_rect.y,
+            WHITE,
+            DrawTextureParams {
+                source: Some(src_rect),
+                dest_size: Some(dest_rect.size()),
+                ..Default::default()
+            },
+        );
     }
 }
