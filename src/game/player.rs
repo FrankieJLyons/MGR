3d8a42f6bfd2a0e44ceb15c0297487b41639483c
Frankie <frankiejlyons@gmail.com>
2023-03-30 01:49:58 +0100

    minor walking refactor

--- src/game/player.rs
+++ src/game/player.rs
@@ -33,9 +33,14 @@ const FS_WALKING: Vec2 = Vec2::new(17.0, 30.0);
 const MF_WALKING: u32 = 2;
 
 impl Player {
+    // Public
     pub async fn new() -> Self {
-        let standing_texture = load_texture("assets/snake/snake_standing.png").await.unwrap();
-        let walking_texture = load_texture("assets/snake/snake_walking.png").await.unwrap();
+        let standing_texture = load_texture("assets/snake/standing.png").await.unwrap();
+        standing_texture.set_filter(FilterMode::Nearest);
+
+        let walking_texture = load_texture("assets/snake/walking.png").await.unwrap();
+        walking_texture.set_filter(FilterMode::Nearest);
+
         Self {
             texture: standing_texture,
             textures: [standing_texture, walking_texture],
@@ -44,7 +49,7 @@ impl Player {
             position: Vec2::new(screen_width() / 2.0, screen_height() / 2.0),
             speed: 2.56,
             frame_counter: 0,
-            frame_delay: Duration::from_millis(250),
+            frame_delay: Duration::from_millis(224),
             last_frame_update: std::time::Instant::now()
         }
     }
@@ -99,36 +104,33 @@ impl Player {
     }
     
     pub fn draw(&self) {
-        let (src_x, src_y) = match self.state {
+        let (src_x, src_y, src_w, src_h) = match self.state {
             State::Standing => {
                 match self.direction {
-                    Direction::Down => (0.0, 0.0),
-                    Direction::Left => (0.0, FS_STANDING.y),
-                    Direction::Up => (0.0, FS_STANDING.y * 2.0),
-                    Direction::Right => (0.0, FS_STANDING.y * 3.0),
+                    Direction::Down => (0.0, 0.0, FS_STANDING.x, FS_STANDING.y),
+                    Direction::Left => (0.0, FS_STANDING.y, FS_STANDING.x, FS_STANDING.y),
+                    Direction::Up => (0.0, FS_STANDING.y * 2.0, FS_STANDING.x, FS_STANDING.y),
+                    Direction::Right => (0.0, FS_STANDING.y * 3.0, FS_STANDING.x, FS_STANDING.y),
                 }
             },
             State::Walking => {
                 let frame = ((self.frame_counter % MF_WALKING)) as f32;
                 match self.direction {
-                    Direction::Down => (FS_WALKING.x * frame, 0.0),
-                    Direction::Left => (FS_WALKING.x * frame, FS_WALKING.y),
-                    Direction::Up => (FS_WALKING.x * frame, FS_WALKING.y * 2.0),
-                    Direction::Right => (FS_WALKING.x * frame, FS_WALKING.y * 3.0),
+                    Direction::Down => (FS_WALKING.x * frame, 0.0, FS_WALKING.x, FS_WALKING.y),
+                    Direction::Left => (FS_WALKING.x * frame, FS_WALKING.y, FS_WALKING.x, FS_WALKING.y),
+                    Direction::Up => (FS_WALKING.x * frame, FS_WALKING.y * 2.0, FS_WALKING.x, FS_WALKING.y),
+                    Direction::Right => (FS_WALKING.x * frame, FS_WALKING.y * 3.0, FS_WALKING.x, FS_WALKING.y),
                 }
             },
         };
 
-        let src_rect = Rect::new(src_x, src_y, FS_STANDING.x, FS_STANDING.y);
-
+        let src_rect = Rect::new(src_x, src_y, src_w, src_h);
         let dest_rect = Rect::new(
             self.position.x,
             self.position.y,
-            FS_STANDING.x * SCALE,
-            FS_STANDING.y * SCALE,
+            src_w * SCALE,
+            src_h * SCALE,
         );
-
-        self.texture.set_filter(FilterMode::Nearest);
 
         draw_texture_ex(
             self.texture,
@@ -143,40 +145,22 @@ impl Player {
         );
     }
 
+    // Private
     fn update_frame_counter(&mut self) -> u32 {
         let now = std::time::Instant::now();
         let elapsed = now - self.last_frame_update;
 
-        let max_frames = if is_key_down(KeyCode::Down)
-            || is_key_down(KeyCode::Left)
-            || is_key_down(KeyCode::Up)
-            || is_key_down(KeyCode::Right)
-        {
-            MF_WALKING * 2
-        } else {
-            MF_WALKING
-        };
-
-        let start_frame = if is_key_down(KeyCode::Down) {
-            0
-        } else if is_key_down(KeyCode::Left) {
-            MF_WALKING
-        } else if is_key_down(KeyCode::Up) {
-            MF_WALKING * 2
-        } else if is_key_down(KeyCode::Right) {
-            MF_WALKING * 3
-        } else {
-            // If no arrow keys are pressed, set start_frame to 0
-            0
+        let max_frames = match self.state {
+            State::Standing => 0,
+            State::Walking => 2
         };
 
         if elapsed >= self.frame_delay {
             let frames = elapsed.as_secs_f32() / self.frame_delay.as_secs_f32();
-            self.frame_counter = (self.frame_counter + frames as u32) % max_frames + start_frame;
+            self.frame_counter = (self.frame_counter + frames as u32) % max_frames;
             self.last_frame_update = now;
-            self.frame_counter
-        } else {
-            self.frame_counter
         }
+
+        self.frame_counter
     }
 }
