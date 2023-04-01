e95fd53d8422ae0e55135bafca5441d3b0697fb6
Frankie <frankiejlyons@gmail.com>
2023-04-01 01:50:32 +0100

    map texture loading

--- src/game.rs
+++ src/game.rs
@@ -1,23 +1,30 @@
 use macroquad::prelude::*;
 
 pub mod player;
+pub mod map;
+
 use self::player::Player;
+use self::map::Map;
 
 pub struct Game {
     player: Player,
+    map: Map,
 }
 
 impl Game {
     pub async fn new() -> Result<Self, FileError> {
         let player = Player::new().await;
-        Ok(Self { player })
+        let map = Map::new().await;
+        
+        Ok(Self { player, map })
     }
 
     pub fn update(&mut self) {
         self.player.update();
     }
 
-    pub fn draw(&self) {
+    pub fn draw(&mut self) {
+        self.map.draw();
         self.player.draw();
     }
 }
