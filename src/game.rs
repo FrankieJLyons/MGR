fdfce7696bc0d296c100e7499d1dd2b9725ea493
Frankie <frankiejlyons@gmail.com>
2023-04-10 17:34:24 +0100

    new room logic

--- src/game.rs
+++ src/game.rs
@@ -5,25 +5,34 @@ pub mod map;
 
 use crate::game::player::Direction;
 
+use self::map::room::Room;
 use self::player::{Player, OFFSET_COL_POS};
 use self::map::Map;
 
 pub struct Game {
     player: Player,
     map: Map,
+    current_room: Room,
+    time_since_last_check: f32,
+    check_interval: f32
 }
 
 impl Game {
     pub async fn new() -> Result<Self, FileError> {
         let player = Player::new().await;
         let map = Map::new("assets/rooms/arrays/b1_f1.txt").await;
+
+        let rooms = &map.rooms;
+        let found_room = rooms.iter().find(|room| room.bounds.contains(player.collider.center()));
+        let current_room = found_room.unwrap().clone();
         
-        Ok(Self { player, map })
+        Ok(Self { player, map, current_room, time_since_last_check: 0.0, check_interval: 1.0})
     }
 
     pub fn update(&mut self) {      
         self.player.update();
 
+        self.room_getter(get_frame_time());
         self.room_collision();
 
         self.camera_update();
@@ -44,63 +53,67 @@ impl Game {
         });
     }
 
+    fn room_getter(&mut self, delta_time: f32) {
+        self.time_since_last_check += delta_time;
+
+        if self.time_since_last_check >= self.check_interval {
+            if !self.current_room.bounds.contains(self.player.collider.center()) {
+                let rooms = &self.map.rooms;
+                let found_room = rooms.iter().find(|room| room.bounds.contains(self.player.collider.center()));
+                self.current_room = found_room.unwrap().clone();
+            }
+        }
+
+    }
+
     fn room_collision(&mut self) {
-        // find the room the player is currently in
-        let rooms = &self.map.rooms;
-        let current_room = rooms.iter().find(|room| room.bounds.contains(self.player.collider.center()));
+        let colliders = &self.current_room.collider_map.colliders;
 
-        // check for collisions with the player's collider
-        if let Some(room) = current_room {
-            let colliders = &room.collider_map.colliders;
+        for collider in colliders {
+            if collider.overlaps(&self.player.collider) { 
+                // Easy maths
+                let col_right = collider.x + collider.w;
+                let col_bottom = collider.y + collider.h;
+                let play_right = self.player.collider.x + self.player.collider.w;
+                let play_bottom = self.player.collider.y + self.player.collider.h;
+                let buffer = 0.5;
 
-            for collider in colliders {
-                if collider.overlaps(&self.player.collider) { 
-                    // Easy maths
-                    let col_right = collider.x + collider.w;
-                    let col_bottom = collider.y + collider.h;
-                    let play_right = self.player.collider.x + self.player.collider.w;
-                    let play_bottom = self.player.collider.y + self.player.collider.h;
-                    let buffer = 0.5;
-
-                    if self.player.direction == Direction::Up {
-                        if self.player.collider.y < col_bottom || self.player.collider.y + self.player.speed < col_bottom {
-                            self.player.position.y = col_bottom - OFFSET_COL_POS + buffer;
-                            self.player.col_arr[0] = true;
-                        } else {
-                            self.player.col_arr[0] = false;
-                        }
-                    } 
-
-                    else if self.player.direction == Direction::Down {
-                        if play_bottom > collider.y || play_bottom - self.player.speed > collider.y  {
-                            self.player.position.y = collider.y - self.player.bounds.h - buffer;
-                            self.player.col_arr[1] = true;
-                        } else {
-                            self.player.col_arr[1] = false;
-                        }
-                    }
-                    
-                    else if self.player.direction == Direction::Left {
-                        if self.player.collider.x < col_right || self.player.collider.x + self.player.speed < col_right  {
-                            self.player.position.x = col_right + buffer;
-                            self.player.col_arr[2] = true;
-                        } else {
-                            self.player.col_arr[2] = false;
-                        }
-                    }
-
-                    else if self.player.direction == Direction::Right {
-                        if play_right > collider.x || play_right - self.player.speed > collider.x  {
-                            self.player.position.x = collider.x - self.player.collider.w - buffer;
-                            self.player.col_arr[3] = true;
-                        } else {
-                            self.player.col_arr[3] = false;
-                        }
+                if self.player.direction == Direction::Up {
+                    if self.player.collider.y < col_bottom || self.player.collider.y + self.player.speed < col_bottom {
+                        self.player.position.y = col_bottom - OFFSET_COL_POS + buffer;
+                        self.player.col_arr[0] = true;
+                    } else {
+                        self.player.col_arr[0] = false;
                     }
                 } 
-            }
-        } else {
-            eprintln!("Out of Bounds: Player Position = {:?}", self.player.position);
+
+                else if self.player.direction == Direction::Down {
+                    if play_bottom > collider.y || play_bottom - self.player.speed > collider.y  {
+                        self.player.position.y = collider.y - self.player.bounds.h - buffer;
+                        self.player.col_arr[1] = true;
+                    } else {
+                        self.player.col_arr[1] = false;
+                    }
+                }
+                
+                else if self.player.direction == Direction::Left {
+                    if self.player.collider.x < col_right || self.player.collider.x + self.player.speed < col_right  {
+                        self.player.position.x = col_right + buffer;
+                        self.player.col_arr[2] = true;
+                    } else {
+                        self.player.col_arr[2] = false;
+                    }
+                }
+
+                else if self.player.direction == Direction::Right {
+                    if play_right > collider.x || play_right - self.player.speed > collider.x  {
+                        self.player.position.x = collider.x - self.player.collider.w - buffer;
+                        self.player.col_arr[3] = true;
+                    } else {
+                        self.player.col_arr[3] = false;
+                    }
+                }
+            } 
         }
     }
 }
